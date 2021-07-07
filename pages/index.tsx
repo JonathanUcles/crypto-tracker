ad54bb61589c66c56646a8a21e460c12b5959f88
JonyUcles <jony.heaply@gmail.com>
2021-07-06 23:28:27 -0700

    fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import IndexView from '../components/views/IndexView'
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <Layout title="Crypto Tracker">
     < IndexView />
   </Layout>
 )
