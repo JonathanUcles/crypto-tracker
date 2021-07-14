76b8ae6d1677962690404d5a772d9835f9b46265
JonyUcles <jony.heaply@gmail.com>
2021-07-14 16:53:42 -0700

    adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { AppProps } from 'next/app'
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 import '../styles/index.css'
 import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
 
-export default MyApp
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
+
+export default App;
